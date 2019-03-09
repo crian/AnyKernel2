9b97550535102904c0aced4403f76503c3c69a9e
crian <crian84@gmail.com>
2019-03-09 13:32:03 +0100

    ramdisk: kronickernel: Add more default settings
    Signed-off-by: crian <crian84@gmail.com>

--- ramdisk/init.kronickernel.sh
+++ ramdisk/init.kronickernel.sh
@@ -30,6 +30,17 @@ echo 128 > /sys/block/sdf/queue/read_ahead_kb
 echo 1 > /sys/block/sdf/queue/iostats
 echo 128 > /sys/block/sdf/queue/nr_requests
 
+# Setup schedutil governor
+echo "schedutil" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+echo 500 > /sys/devices/system/cpu/cpufreq/policy0/schedutil/up_rate_limit_us
+echo 20000 > /sys/devices/system/cpu/cpufreq/policy0/schedutil/down_rate_limit_us
+echo 1 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/iowait_boost_enable
+
+echo "schedutil" > /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor	
+echo 500 > /sys/devices/system/cpu/cpufreq/policy4/schedutil/up_rate_limit_us
+echo 20000 > /sys/devices/system/cpu/cpufreq/policy4/schedutil/down_rate_limit_us
+echo 1 > /sys/devices/system/cpu/cpu4/cpufreq/schedutil/iowait_boost_enable
+
 # Set cpu input boost
 echo 100 > /sys/module/cpu_input_boost/parameters/input_boost_duration
 echo 979200 > /sys/module/cpu_input_boost/parameters/input_boost_freq_hp
@@ -40,7 +51,25 @@ echo 300000 > /sys/module/cpu_input_boost/parameters/remove_input_boost_freq_lp
 # Set dynamic stune boost
 echo 15 > /sys/module/cpu_input_boost/parameters/dynamic_stune_boost
 
+# Set schedtune for foreground/top-app
+echo 1 > /dev/stune/foreground/schedtune.prefer_idle
+echo 0 > /dev/stune/top-app/schedtune.boost
+echo 1 > /dev/stune/top-app/schedtune.prefer_idle
+
+# Setup cpusets for better load balancing
+echo 0-7 > /dev/cpuset/top-app/cpus
+echo 0-3,6-7 > /dev/cpuset/foreground/boost/cpus
+echo 0-3,6-7 > /dev/cpuset/foreground/cpus
+echo 0-1 > /dev/cpuset/background/cpus
+echo 0-3  > /dev/cpuset/system-background/cpus
+
+# For better screen off idle
+echo 0-3 > /dev/cpuset/restricted/cpus
+
+# Adjust lmk
+echo "18432,23040,27648,32256,55296,80640" > /sys/module/lowmemorykiller/parameters/minfree
+
 # Configure virtual memory
-echo 65 > /proc/sys/vm/swappiness
+echo 8 > /proc/sys/vm/swappiness
 echo 5 > /proc/sys/vm/dirty_ratio
 echo 2 > /proc/sys/vm/dirty_background_ratio
