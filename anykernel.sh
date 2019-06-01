42611c26be4128575d9c0a211cc897bf78a57266
osm0sis <osm0sis@outlook.com>
2019-06-01 00:00:17 -0300

    anykernel: Update for AK3

--- anykernel.sh
+++ anykernel.sh
@@ -1,4 +1,4 @@
-# AnyKernel2 Ramdisk Mod Script
+# AnyKernel3 Ramdisk Mod Script
 # osm0sis @ xda-developers
 
 ## AnyKernel setup
@@ -24,7 +24,7 @@ ramdisk_compression=auto;
 
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
-. /tmp/anykernel/tools/ak2-core.sh;
+. tools/ak3-core.sh;
 
 
 ## AnyKernel file attributes
@@ -44,6 +44,5 @@ remove_section init.rc "service flash_recovery" ""
 # end ramdisk changes
 
 write_boot;
-
 ## end install
 
